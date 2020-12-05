38e3fc0a22079348e78500f734ec6b08bb261af1
dmlesc <dmlesc@dmlesc>
2020-12-04 19:05:50 -0700

    refactor

--- src/sensor_exporter.js
+++ src/sensor_exporter.js
@@ -1,84 +1,67 @@
 'use strict'
-process.on('uncaughtException', (err) => { console.log('uncaught', err.stack); })
+process.on('uncaughtException', (err) => { console.log('uncaught: ', err.stack) })
 
+const http = require('http')
 const { spawn } = require('child_process')
 
 const port = 9113
 const ip = '0.0.0.0'
-const http = require('http')
 const prefix = 'sensor_'
 
 var register = {}
 
-var metrics = {}
-
-metrics.register = (name) => {
-  register[name] = {value:0}
-}
-metrics.set = (name, value) => {
-  register[name]['value'] = value
+function register_metric (name) {
+  register[name] = 0
 }
 
-metrics.register('temperature_celcius')
-metrics.register('temperature_fahrenheit')
-metrics.register('humidity')
-
-metrics.server = () => {  
-
-  const server = http.createServer((req, res) => {
-    var http_status_code = 200
-    var data = ''
-  
-    if (req.url == '/metrics') {
-      const sensor = spawn('python', ['sensor.py'])
-
-      sensor.stdout.on('data', (data) => {
-        var sensor_data = JSON.parse(data)
-
-        metrics.set('temperature_celcius', sensor_data.temperature_celcius)
-        metrics.set('temperature_fahrenheit', sensor_data.temperature_fahrenheit)
-        metrics.set('humidity', sensor_data.humidity)
-      })
-
-      sensor.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`)
-      })
-
-      sensor.on('close', (code) => {
-        // console.log(`exit code ${code}`)
-
-        data += collectMetrics()
-
-        res.writeHead(http_status_code, { 'Content-Type': 'text/plain' })
-        res.end(data)
-      })
-
-    }
-    else {
-      http_status_code = 404
-      data = 'not found'
-
-      res.writeHead(http_status_code, { 'Content-Type': 'text/plain' })
-      res.end(data)
-    }
-  })
-  
-  server.listen(port, ip)
-  console.log('metrics', 'ready to serve metrics')
+function set_metric (name, value) {
+  register[name] = value
 }
 
-
-function collectMetrics() {
+function metrics_assemble () {
   var text = ''
         
   Object.keys(register).forEach( (metric_name) => {
     var name = prefix + metric_name
-    var metric = register[metric_name]
-
-    text += name + ' ' + metric['value'] + '\n'
+    text += name + ' ' + register[metric_name] + '\n'
   })
 
   return text
 }
 
-metrics.server()
+
+register_metric('temperature_celcius')
+register_metric('temperature_fahrenheit')
+register_metric('humidity')
+
+
+const server = http.createServer((req, res) => {
+  if (req.url == '/metrics') {
+    const sensor = spawn('python', ['sensor.py'])
+
+    sensor.stdout.on('data', (data) => {
+      var sensor_data = JSON.parse(data)
+
+      set_metric('temperature_celcius', sensor_data.temperature_celcius)
+      set_metric('temperature_fahrenheit', sensor_data.temperature_fahrenheit)
+      set_metric('humidity', sensor_data.humidity)
+    })
+
+    sensor.stderr.on('data', (data) => { console.error(`stderr: ${data}`) })
+
+    sensor.on('close', (code) => { // console.log(`exit code ${code}`)
+      var response_text = metrics_assemble()
+
+      res.writeHead(200, { 'Content-Type': 'text/plain' })
+      res.end(response_text)
+    })
+
+  }
+  else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' })
+    res.end('not found')
+  }
+})
+
+server.listen(port, ip)
+console.log('ready to serve sensor data')
